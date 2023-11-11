095e44dfe4bef9f932fb851e26542d09332ee891
Mateusz <mateusz.rybczynski@icloud.com>
2023-11-11 21:55:25 +0100

    Initial Commit

---
+++ Chat-app/Chat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_appApp.swift
+//  Chat-app
+//
+//  Created by Mateusz Rybczyński on 11/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Chat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
