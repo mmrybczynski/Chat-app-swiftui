01df75e6808b36745588d4007603e71bb3d2ab3b
Mateusz <mateusz.rybczynski@icloud.com>
2023-11-11 22:01:33 +0100

    Add icon and first component

---
+++ Chat-app/Components/TitleRow.swift
@@ -0,0 +1,18 @@
+//
+//  TitleRow.swift
+//  Chat-app
+//
+//  Created by Mateusz Rybczyński on 11/11/2023.
+//
+
+import SwiftUI
+
+struct TitleRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleRow()
+}
