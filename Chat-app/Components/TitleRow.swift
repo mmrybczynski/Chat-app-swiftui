2b293557bdd80dea54e75144d184e1f9855029df
Mateusz <mateusz.rybczynski@icloud.com>
2023-11-12 19:58:58 +0100

    Created titlerow view

--- Chat-app/Components/TitleRow.swift
+++ Chat-app/Components/TitleRow.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct TitleRow: View {
+    var imageURL = URL(string: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+    var name:String = "Matthew Cook"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20) {
+            AsyncImage(url: imageURL) {image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 50, height: 50)
+                    .cornerRadius(50)
+            } placeholder: {
+                ProgressView()
+            }
+            
+            VStack(alignment: .leading) {
+                Text(name)
+                    .font(.title2)
+                
+                Text("Online")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image(systemName: "phone.fill")
+                .foregroundColor(.gray)
+                .padding()
+                .background(.white)
+                .cornerRadius(50)
+        }
+        .padding()
     }
 }
 
