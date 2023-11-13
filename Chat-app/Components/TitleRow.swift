9db77c78e1c6f7f42c42ef0b661a45f6c1beef36
Mateusz <mateusz.rybczynski@icloud.com>
2023-11-13 23:06:22 +0100

    Modify content view

--- Chat-app/Components/TitleRow.swift
+++ Chat-app/Components/TitleRow.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct TitleRow: View {
     var imageURL = URL(string: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-    var name:String = "Matthew Cook"
+    var name:String = "Natalie Cook"
     var body: some View {
         HStack(spacing: 20) {
             AsyncImage(url: imageURL) {image in
