1ffa4ecae56e44e2f0990f33da1d77cec61810fa
Mateusz <mateusz.rybczynski@icloud.com>
2023-11-17 20:05:21 +0100

    Import Furebase to project

--- Chat-app/Chat_appApp.swift
+++ Chat-app/Chat_appApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Chat_appApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
